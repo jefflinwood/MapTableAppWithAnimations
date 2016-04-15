4b5c342c3aaea5f9cb76d97770222aba7ecd4579
Jeffrey Linwood <jlinwood@gmail.com>
2016-04-15 09:55:01 -0500

    Actual App

--- MapTableAppWithAnimations/ViewController.swift
+++ MapTableAppWithAnimations/ViewController.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import MapKit
 
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var slidingDetailTitleLabel: UILabel!
+    
+    @IBOutlet var slidingDetailBottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +28,39 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func dismissSlidingDetail(sender: AnyObject) {
+        self.view.layoutIfNeeded()
+        self.slidingDetailBottomConstraint.constant = -400
+        
+        UIView.animateWithDuration(2.0, delay: 0.0, options: .CurveEaseInOut, animations: {
+            self.view.layoutIfNeeded()
+            }, completion: nil)
 
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PlaceCell")
+        cell?.textLabel?.text = "Row \(indexPath.row)"
+        return cell!
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        self.slidingDetailTitleLabel.text =  "Row \(indexPath.row)"
+        self.view.layoutIfNeeded()
+        self.slidingDetailBottomConstraint.constant = 0
+
+        UIView.animateWithDuration(2.0, delay: 0.0, options: .CurveEaseInOut, animations: {
+            self.view.layoutIfNeeded()
+            }, completion: nil)
+    }
 
 }
 
